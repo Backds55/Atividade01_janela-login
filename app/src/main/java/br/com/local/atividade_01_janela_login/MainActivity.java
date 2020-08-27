1b5925890cb53baa226344bc301534bcc1a15a6e
Backds55 <luihiroshi6@gmail.com>
2020-08-27 13:30:34 -0300

    Atividade 01 - PAMI - 27_08 - janela/login - com toolbar main;

---
+++ app/src/main/java/br/com/local/atividade_01_janela_login/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.atividade_01_janela_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
